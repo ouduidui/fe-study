608a9ca8d5867b7588d2d7cbdfbc73654b9faf50
oushihao <oushihao97@163.com>
2021-10-10 16:33:29 +0800

    feat: 实现HTTP学习DEMO

---
+++ package/http/http-demo/http-server.ts
@@ -0,0 +1,16 @@
+import {createServer} from "http";
+
+// 创建一个http server
+const httpServer = createServer((req, res) => {
+    // 设置状态码
+    res.statusCode = 200;
+    // 设置响应头
+    res.setHeader('Content-type', 'text/html');
+    // 返回响应内容
+    res.end('<h1>HelloWorld</h1>')
+})
+
+// 监听端口
+httpServer.listen(8080, () => {
+    console.log('Server Running')
+})
