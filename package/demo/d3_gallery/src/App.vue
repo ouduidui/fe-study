950192e499d61204fa7ef3a008d3a869b84aa63c
oushihao <oushihao97@163.com>
2021-10-29 11:27:05 +0800

    feat(d3-gallery): 实现简易柱状图

---
+++ package/demo/d3_gallery/src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import histogram from "./components/histogram.vue"
+</script>
+
+<template>
+  <div class="page">
+    <histogram />
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+}
+
+.page {
+  padding: 30px;
+}
+</style>
