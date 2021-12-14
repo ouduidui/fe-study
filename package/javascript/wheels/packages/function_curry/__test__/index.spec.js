64fb0bb2fa3323fd77f8d785ccf911afb993fbb0
oushihao <oushihao97@163.com>
2021-12-14 17:31:04 +0800

    feat(js-wheels): 实现函数柯里化

---
+++ package/javascript/wheels/packages/function_curry/__test__/index.spec.js
@@ -0,0 +1,16 @@
+const curry = require('../src/index');
+
+describe('函数柯里化', () => {
+  it('happy path', () => {
+    function add(a, b, c, d) {
+      return a + b + c + d;
+    }
+
+    const add2 = curry(add, 1);
+
+    expect(typeof add2 === 'function').toBe(true);
+    expect(add2(2)(3)(4)).toBe(add(1, 2, 3, 4));
+    expect(add2(2, 3)(4)).toBe(add(1, 2, 3, 4));
+    expect(add2(2)(3, 4)).toBe(add(1, 2, 3, 4));
+  })
+})
---
+++ package/javascript/wheels/packages/function_curry/src/index.js
@@ -0,0 +1,26 @@
+/**
+ *
+ * @param fn {Function}
+ * @param args {any}
+ * @returns {(function(...[*]): void)|*}
+ */
+function curry(fn, ...args) {
+  // 获取原函数的参数长度
+  let length = fn.length;
+
+  return function (...newArgs) {
+    // 合并参数
+    const combined = [...args, ...newArgs];
+
+    // 判断参数总和是否达到要求
+    if(combined.length < length) {
+      // 如果还是缺少参数，则返回函数继续调用
+      return curry(fn.bind(this), ...combined);
+    }else {
+      // 如果参数不缺少，就调用函数
+      return fn.apply(this, combined);
+    }
+  }
+}
+
+module.exports = curry;
