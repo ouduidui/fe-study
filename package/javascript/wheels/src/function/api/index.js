ca6bf5a269ce2eb95733d1a121da87b712b25087
oushihao <oushihao97@163.com>
2022-03-11 10:46:02 +0800

    feat: split js-wheels to a new repository

--- package/javascript/wheels/src/function/api/index.js
+++
@@ -1,5 +0,0 @@
-Function.prototype._call = require('./call');
-Function.prototype._apply = require('./apply');
-Function.prototype._bind = require('./bind');
-
-module.exports = Function;
--- package/javascript/wheels/src/string/api/index.js
+++
@@ -1,7 +0,0 @@
-const trim = require('./trim');
-const slice = require('./slice');
-
-String.prototype._trim = trim;
-String.prototype._slice = slice;
-
-module.exports = String;
